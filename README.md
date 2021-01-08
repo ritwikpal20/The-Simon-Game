18a3b2bddec733d704083bb44a4d4a3c08e0ab38
Ritwik Pal <ritwikpal20@gmail.com>
2021-01-08 16:21:16 +0530

    sounds added and minute fixes

---
+++ README.md
@@ -0,0 +1,5 @@
+# The Simon Game
+
+### For each level , colors with played along with the sound . Once all the colors are played , you have to select them in correct order as shown earlier. Difficulty increases in each level by 1. If you select colors in the wrong order , you lose the game and the score earned will be displayed.
+
+## Play the game at : https://ritwikpal20.github.io/The-Simon-Game/
--- game.js
+++ game.js
@@ -3,6 +3,7 @@ buttonColors = ["red", "green", "yellow", "blue"];
 gameOver = true;
 
 function generateSequence(gameLevel, matchSequence) {
+    $(".restart").hide();
     $(".btn").off("click");
     sequenceOfColors = [];
     clickNumber = 0;
@@ -11,6 +12,8 @@ function generateSequence(gameLevel, matchSequence) {
         if (i != gameLevel) {
             randomNumber = Math.floor(Math.random() * 3) + 1;
             $(`#${buttonColors[randomNumber]}`).addClass("pressed-computer");
+            colorSound = new Audio(`sounds/${buttonColors[randomNumber]}.mp3`);
+            colorSound.play();
             setTimeout(() => {
                 $(`#${buttonColors[randomNumber]}`).removeClass(
                     "pressed-computer"
@@ -22,24 +25,40 @@ function generateSequence(gameLevel, matchSequence) {
             matchSequence();
         }
         i++;
-    }, 600);
+    }, 800);
 }
 
 function matchSequence() {
     $(".btn").click(function () {
         btnColor = $(this).attr("id");
         $(this).addClass("pressed-player");
+        if (gameOver == true) {
+            colorSound = new Audio(`sounds/wrong.mp3`);
+            colorSound.play();
+        } else {
+            colorSound = new Audio(`sounds/${btnColor}.mp3`);
+            colorSound.play();
+        }
         setTimeout(() => {
             $(this).removeClass("pressed-player");
         }, 200);
-        if (sequenceOfColors[clickNumber] == btnColor) {
+        if (sequenceOfColors[clickNumber] == btnColor && gameOver == false) {
             clickNumber++;
         } else {
             gameOver = true;
-            $("h1").text("Game Over.Press a key to play again.");
+            $("body").addClass("game-over");
+            setTimeout(() => {
+                $("body").removeClass("game-over");
+            }, 200);
+            $("h1").text(`Game Over. Your Score:${gameLevel}`);
+            $(".restart").show();
         }
         if (clickNumber == sequenceOfColors.length) {
-            startGame();
+            $("h1").text("Correct");
+            $(".btn").off("click");
+            setTimeout(() => {
+                startGame();
+            }, 1000);
         }
     });
 }
@@ -48,14 +67,19 @@ function startGame() {
     if (gameOver == true) {
         gameLevel = 1;
         gameOver = false;
+        $(".restart").show();
     } else {
         gameLevel++;
     }
     $("h1").text(`Level:${gameLevel}`);
     generateSequence(gameLevel, matchSequence);
-    // matchSequence();
 }
 
+function addSound() {}
+
 $(document).keypress(() => {
+    if (gameOver == true) startGame();
+});
+$(".restart").on("click", () => {
     if (gameOver == true) startGame();
 });
